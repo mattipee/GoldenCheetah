3ebc7ce7127e5d1a83abd2fff3dfedff393fa56b
mattipee <mattipee@yahoo.co.uk>
2020-12-20 20:58:47 +0000

    Dont's call usb_set_altinterface on LINUX, was causing ETIMEDOUT for me

--- src/Train/LibUsb.cpp
+++ src/Train/LibUsb.cpp
@@ -444,7 +444,7 @@ usb_dev_handle* LibUsb::OpenFortius()
                                 if (rc < 0) {
                                     qDebug()<<"usb_claim_interface Error: "<< usb_strerror();
                                 } else {
-                                    if (OperatingSystem != OSX) {
+                                    if (OperatingSystem != OSX && OperatingSystem != LINUX) {
                                         // fails on Mac OS X, we don't actually need it anyway
                                         rc = usb_set_altinterface(udev, alternate);
                                         if (rc < 0) qDebug()<<"usb_set_altinterface Error: "<< usb_strerror();
